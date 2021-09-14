07b427684785298569f8ffbb5fd2580e87836b52
Facundo Bernal <fvbernal222@gmail.com>
2021-09-14 00:35:37 -0300

    Primer desafio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let numero1 = prompt("Ingresá un número");
+let numero2 = prompt("Ingresá otro número");
+
+let suma = parseInt(numero1) + parseInt(numero2);
+let resta = parseInt(numero1) - parseInt(numero2);
+let multiplicacion = parseInt(numero1) * parseInt(numero2);
+let division = parseInt(numero1) / parseInt(numero2);
+let resto = parseInt(numero1) % parseInt(numero2);
+
+alert("El resultado de la suma es " + suma);
+alert("El resultado de la resta es " + resta);
+alert("El resultado de la multiplicacion es " + multiplicacion);
+alert("El resultado de la division es " + division);
+alert("El resultado del resto es " + resto);
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+console.log(resto);
